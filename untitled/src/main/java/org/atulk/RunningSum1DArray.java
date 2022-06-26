3526d5f8735b3eea77af14aa598baeef66f43374
Atul Kumar <atul.1.kumar@oracle.com>
2022-06-26 08:47:12 +0530

    removing commented code

--- untitled/src/main/java/org/atulk/RunningSum1DArray.java
+++ untitled/src/main/java/org/atulk/RunningSum1DArray.java
@@ -20,8 +20,6 @@ public class RunningSum1DArray {
     }
 
     public static int[] runningSumInPlace(int[] nums) {
-//        int[] ans = new int[nums.length];
-//        ans[0] = nums[0];
         for (int i = 1; i < nums.length; i++)
             nums[i] = nums[i-1] + nums[i];
         return nums;
